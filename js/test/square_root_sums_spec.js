db50c0db2d54dcc59980ce7938e5945fe58f937c
maliaoreta <maliaoreta@gmail.com>
2016-03-25 09:30:48 -1000

    altered test to check if returning number separately

--- js/test/square_root_sums_spec.js
+++ js/test/square_root_sums_spec.js
@@ -5,7 +5,7 @@ describe('squareRootSums', function () {
     expect(squareRootSums).to.be.a('function');
   });
 
-  it('should take a single input of type Number and return the sum of the square root of every number from 1 to the givn number', function () {
+  it('should return a number', function () {
 
     expect(squareRootSums(4)).to.be.a('number');
   })
