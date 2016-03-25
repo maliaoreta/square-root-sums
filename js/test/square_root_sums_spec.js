3cc360a1d8fbd6cefe48104a01c66a097f8b4dff
maliaoreta <maliaoreta@gmail.com>
2016-03-25 09:28:58 -1000

    checks if squareRootSums returns a number

--- js/test/square_root_sums_spec.js
+++ js/test/square_root_sums_spec.js
@@ -4,4 +4,9 @@ describe('squareRootSums', function () {
 
     expect(squareRootSums).to.be.a('function');
   });
+
+  it('should take a single input of type Number and return the sum of the square root of every number from 1 to the givn number', function () {
+
+    expect(squareRootSums(4)).to.be.a('number');
+  })
 });
