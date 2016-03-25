7898b141e90e93b5fed722a71ebdbaf97633dec8
maliaoreta <maliaoreta@gmail.com>
2016-03-25 09:37:40 -1000

    checks squareRootSums result using 4 as a test value

--- js/test/square_root_sums_spec.js
+++ js/test/square_root_sums_spec.js
@@ -8,5 +8,10 @@ describe('squareRootSums', function () {
   it('should return a number', function () {
 
     expect(squareRootSums(4)).to.be.a('number');
+  });
+
+  it('should return the sum of the square roots of 1 to the given number', function () {
+
+    expect(squareRootSums(4)).to.equal(30);
   })
 });
