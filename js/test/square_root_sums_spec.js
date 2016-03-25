cc507dea5d5cd9c3b4094edb9a5c7aa0a78ade03
maliaoreta <maliaoreta@gmail.com>
2016-03-25 10:11:01 -1000

    fixed to test for sum of square roots of 4 rather than squares

--- js/test/square_root_sums_spec.js
+++ js/test/square_root_sums_spec.js
@@ -13,10 +13,6 @@ describe('squareRootSums', function () {
   it('should return the sum of the square roots of 1 to the given number', function () {
 
     var test4 = squareRootSums(4);
-    expect(test4).to.equal(30);
-    var test10 = squareRootSums(10);
-    expect(test10).to.equal(385);
-    var test16 = squareRootSums(16);
-    expect(test16).to.equal(1496); 
+    expect(test4).to.equal(6.1462643699419723);
   })
 });
