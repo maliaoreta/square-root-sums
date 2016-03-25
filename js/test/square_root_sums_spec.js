46f29c94c69977ddb0cfbcc74962973e302af525
maliaoreta <maliaoreta@gmail.com>
2016-03-25 09:48:06 -1000

    adds more tests

--- js/test/square_root_sums_spec.js
+++ js/test/square_root_sums_spec.js
@@ -7,11 +7,16 @@ describe('squareRootSums', function () {
 
   it('should return a number', function () {
 
-    expect(squareRootSums(4)).to.be.a('number');
+    expect(squareRootSums()).to.be.a('number');
   });
 
   it('should return the sum of the square roots of 1 to the given number', function () {
 
-    expect(squareRootSums(4)).to.equal(30);
+    var test4 = squareRootSums(4);
+    expect(test4).to.equal(30);
+    var test10 = squareRootSums(10);
+    expect(test10).to.equal(385);
+    var test16 = squareRootSums(16);
+    expect(test16).to.equal(1496);
   })
 });
